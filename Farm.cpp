52d7172c85b1f03d0f87f70e32fda0e45d4321a2
a1818124 <110045635+a1818124@users.noreply.github.com>
2022-10-14 21:12:20 +1030

    Add files via upload

---
+++ Farm.cpp
@@ -0,0 +1,66 @@
+#include "Farm.h"
+#include <iostream>
+using namespace std;
+
+Farm::Farm() { plants = new Plant[10]; }
+
+void Farm::add_plant(Plant p)
+{
+    /* ... insert code here to check if Player has Plant p seed*/
+    // check if there is space/field avalible in farm
+    for (int i = 0; i < 10; i++)
+    {
+        if (plants[i].getGrowth() == -1)
+        {
+            plants[i] = p;
+            /* ... insert code here to remove Plant p seed from Player's inventory*/
+            cout << p.getName() << " is planted in field_" << i << "!\n";
+            return;
+        }
+    }
+    cout << "failed: no room to plant\n";
+}
+
+void Farm::rem_plant(int num)
+{
+    Plant p;
+    if (plants[num].getGrowth() < 10)
+    {
+        cout << plants[num].getName() << " is plucked out from field_" << num << endl;
+        plants[num] = p;
+    }
+    else
+    {
+        cout << plants[num].getName() << " is cultivated from field_" << num << endl;
+        /*... insert code to add plants[num] to Player's inventory*/
+        plants[num] = p;
+    }
+}
+
+Plant *Farm::return_plants() { return plants; }
+
+void Farm::printFarm()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        if (plants[i].getGrowth() != -1)
+            cout << "field_" << i << ": " << plants[i].getName() << "|" << plants[i].getGrowth() << "|$" << plants[i].getPrice() << endl;
+    }
+}
+
+int Farm::getRipeNum()
+{
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (plants[i].getGrowth() >= 10)
+            sum++;
+    }
+    return sum;
+}
+
+Farm::~Farm()
+{
+    cout << "destructor_Farm: delete[] plants\n";
+    delete[] plants;
+}
---
+++ Farm.h
@@ -0,0 +1,35 @@
+#ifndef _FARM_H
+#define _FARM_H
+#include "Plant.h"
+#include "Supply.h"
+
+class Farm
+{
+protected:
+    // the attributes of Farm
+    int growth_factor; // ... to be implemented
+    Plant *plants; // in plan doc: Plant
+    // only in plan doc: Sup supplies;
+public:
+    // constructors for the Farm class
+    Farm();
+
+    // functions that add & remove plant from array
+    void add_plant(Plant p);
+    void rem_plant(int num); // in plan doc: void rem_plant(Plant p). I change argument to int (representing field num) because there can be mutiple apples planted at different time, and rem_plant(apple) doesn't specify which apple to remove
+
+    // a function that returns plants array
+    Plant *return_plants();
+
+    // a function that print what is in the farm
+    void printFarm(); // not in plan doc
+
+    // a function that returns the num of ripe plants in farm (Yang)
+    int getRipeNum(); // not in plan doc
+
+    // destructor for the Farm class
+    ~Farm();
+
+    // only in plan doc: void add_sup(Sup s);
+};
+#endif
